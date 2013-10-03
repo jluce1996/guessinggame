bcf73f1868faef82aaa7ad93a30c6d2530199172
jluce1996 <jluce1996@gmail.com>
2013-10-03 08:42:57 -0400

    guesses the numbers

---
+++ GuessingGame/GuessingGame/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int guess;
+            int a = 1;
+            int b = 100;
+            string input;
+            
+            Console.WriteLine("Think of a number between 1 and 100");
+            while (true)
+            {
+                guess = (a + b) / 2;
+                Console.WriteLine("Is your number {0}", guess);
+                Console.WriteLine("h, l, or e");
+                input = Console.ReadLine();  
+                if (input == "h")
+                {
+                    a = guess + 1;
+                }
+                if (input == "l")
+                {
+                    b = guess - 1;
+                }
+                if (input == "e")
+                {
+                    break;
+                }
+
+            }
+        }
+    }
+}
